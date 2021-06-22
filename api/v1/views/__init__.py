24bbc08db55810ae17f0f782081c62897837bf1e
chrisvanndy <chrisvannyc@gmail.com>
2021-06-22 22:05:43 +0000

    editing url_prefix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from api.v1.views.index import *
 app_views = Blueprint('app_views', __name__,
-                      template_folder= templates)
+                      template_folder= templates,
+                      url_prefix='/api/v1')
 
 @BNB3_route('/api/v1')
+
