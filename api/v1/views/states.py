9ec624efb2c3584bf5037e67d8aa8d6a361c6686
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-23 20:52:09 +0000

    Adding puts code to state id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -42,4 +42,17 @@ def states_by_id(state_id=None):
     
     elif request.method == "DELETE":
         all_states[key].delete()
+        storage.save()
         return jsonify({}), 200
+
+    else request.method == "PUT":
+        json_dict = request.get_json()
+        if not json_dict:
+            abort(400, 'Not a JSON')
+        for k,v in json_dict.items():
+            if k == 'id' or k == 'created_at' or k == 'updated_at':
+                continue
+            else:
+                setattr(all_states[key], k, v)
+        all_states[key].save()
+        return jsonify(BaseModel.to_dict(all_states[key])), 200
