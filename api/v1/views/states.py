4dc5b990250d25d42ec202633c156578c54459de
chrisvanndy <chrisvannyc@gmail.com>
2021-06-23 18:32:02 +0000

    adding states

---
+++ api/v1/views/states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Retrieves a list of all State objects"""
+
+from flask import Flask, Blueprint, render_template, request, url_for, redirect, abort, jsonify, json
+from models.state import State
+from models.base_model import BaseModel
+from api.v1.views import app_views
+from models import storage
+from console import HBNBCommand
+
+@app_views.route('/states', methods=['GET', 'POST'])
+def all_states():
+    if request.method == "GET":
+        all_states = storage.all(State)
+        list_of_dicts = []
+        for values in all_states.values():
+            list_of_dicts.append(BaseModel.to_dict(values))
+    return jsonify(list_of_dicts)
+    else:
+        json_dict = request.get_json()
+        if not json_dict:
+            abort(400, 'Not a JSON')
+        new_dict = json.load(json_dict)
+        if 'name' not in new_dict.key():
+            abort(400, 'Missing name')
+        else:
+           new_state = State(name=new_dict['name'])
