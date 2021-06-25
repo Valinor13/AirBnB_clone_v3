a883ed47b1728e2c0daa3df30a5f78128c77ee36
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-25 15:19:13 +0000

    Undoing changes

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -70,3 +70,52 @@ test_db_storage.py'])
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test the FileStorage class"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_3(self):
+        """ this tests the number 3 """
+        self.assertEqual(3, 3)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_cheese(self):
+        """ this tests the cheeze """
+        self.assertIsInstance("cheese", str)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_all_returns_dict(self):
+        """Test that all returns a dictionaty"""
+        self.assertIs(type(models.storage.all()), dict)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_all_no_class(self):
+        """Test that all returns all rows when no class is passed"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_new(self):
+        """test that new adds an object to the database"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_db_func_get(self):
+        """ Test to ensure calling get() returns an ojbect"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create State name='CaliforniaLand'")
+            CL = f.getvalue()
+        new_obj = DBStorage().get(State, CL[:-2])
+        self.assertIsInstance(new_obj, State)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_db_func_count(self):
+        """ Test to ensure correct count of class instances """
+        count1 = DBStorage().count(State)
+        new_state = State(name="TestLand")
+        new_state.save()
+        count2 = DBStorage().count(State)
+        self.assertIsEqual(count2, count1 + 1)
